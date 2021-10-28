71b549646307786f40e7bbf86dfb32ee9a68bba3
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-10-28 22:35:01 +0300

    [Transformation] Add transformation example

--- src/textureBox/textureBox.cpp
+++ src/textureBox/textureBox.cpp
@@ -128,9 +128,9 @@ int main() {
     glUseProgram(shader);
 
     unsigned int texture1 = bindTextureRGB(
-            "/home/mlgmag/CLionProjects/graphicsLabs/src/textureTriangle/img/container.jpg");
+            "/home/mlgmag/CLionProjects/graphicsLabs/src/textureBox/img/container.jpg");
     unsigned int texture2 = bindTextureRGBA(
-            "/home/mlgmag/CLionProjects/graphicsLabs/src/textureTriangle/img/awesomeface.png");
+            "/home/mlgmag/CLionProjects/graphicsLabs/src/textureBox/img/awesomeface.png");
 
     int texture1UniformLocation = glGetUniformLocation(shader, "texture1");
     glUniform1i(texture1UniformLocation, 0);
