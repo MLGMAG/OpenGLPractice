0a7f00f20e8a1007a9d1400da78a898c160b3dbe
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-12-11 19:15:02 +0200

    [PhongLight] Full

--- src/3d/colorCubeCamera/colorCubeCamera.cpp
+++ src/3d/colorCubeCamera/colorCubeCamera.cpp
@@ -149,7 +149,8 @@ void graphicLogic(const GraphicContext &context) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         auto model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.0f));
         shaderProgram.setMat4("model", model);
 
         glBindVertexArray(cubeVertexArray);
